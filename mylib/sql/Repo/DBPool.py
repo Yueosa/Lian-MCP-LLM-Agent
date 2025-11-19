9ad4ef0945120759f65cb3548e1bffdf6ef9aab5
ReverieX <yichengxin7@gmail.com>
2025-11-19 19:17:39 +0800

    feat(Repo): 关于Repo的新实现
    - 现在由BaseRepo作为统一父类实现各功能

---
+++ mylib/sql/Repo/DBPool.py
@@ -0,0 +1,57 @@
+from psycopg2 import pool
+from typing import Dict, Any
+
+
+
+class PostgreSQLConnectionPool:
+    """PostgreSQL 数据库连接池管理类
+    
+    封装了 psycopg2 的 ThreadedConnectionPool，提供连接的获取和释放功能。
+    """
+    
+    def __init__(self, minconn=1, maxconn=10, **kwargs):
+        """初始化连接池
+        
+        Args:
+            minconn: 连接池中的最小连接数
+            maxconn: 连接池中的最大连接数
+            **kwargs: 传递给 psycopg2 的连接参数，如 host, port, dbname, user, password 等
+        """
+        self._pool = pool.ThreadedConnectionPool(
+            minconn=minconn,
+            maxconn=maxconn,
+            **kwargs
+        )
+        self._kwargs = kwargs
+
+    def get_connection(self):
+        """获取数据库连接
+        
+        Returns:
+            数据库连接对象
+        """
+        return self._pool.getconn()
+
+    def release_connection(self, conn):
+        """释放数据库连接回连接池
+        
+        Args:
+            conn: 要释放的数据库连接对象
+        """
+        self._pool.putconn(conn)
+
+    def clear_connections(self):
+        """关闭所有连接
+        
+        清空连接池中的所有连接。
+        """
+        self._pool.closeall()
+
+    @property
+    def pool_size(self) -> int:
+        """获取连接池的最大连接数
+        
+        Returns:
+            连接池的最大连接数
+        """
+        return self._pool.maxconn
---
+++ mylib/sql/Repo/TaskStepsRepo.py
@@ -0,0 +1,23 @@
+from .BaseRepo import BaseRepo
+from ..Model.TaskSteps import TaskStep
+from ..Sql.SqlLoader import get_sql_loader
+
+
+class TaskStepsRepo(BaseRepo):
+    """task_steps 表的数据库操作类"""
+
+    _sql_loader = get_sql_loader()
+    _table_name = "task_steps"
+    _table_meta = _sql_loader.get_table_meta(_table_name)
+
+    CREATE_TABLE_SQL = _sql_loader.get_table_create_sql(_table_name)
+
+    _allowed_get_fields = _table_meta.get_allowed_fields() if _table_meta else [
+        "id", "task_id", "step_index", "instruction", "output", "status", "created_at", "updated_at"
+    ]
+
+    _model_class = TaskStep
+
+    def get_table_name(self) -> str:
+        """获取表名"""
+        return self._table_name
