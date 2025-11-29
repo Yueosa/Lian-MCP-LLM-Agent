7bd3fc73bd34e26c394ed13d44f0f771cf765cea
Sakurine <yichengxin7@gmail.com>
2025-11-29 17:02:30 +0800

    feat(Lparser): 修改 LParserBase 类，继承自 LPDA 而非 LStateMachine

--- mylib/utils/Lparser/base.py
+++ mylib/utils/Lparser/base.py
@@ -1,16 +1,16 @@
 from typing import Any, Optional
-from mylib.utils.Lfsm.machine import LStateMachine
+from mylib.utils.Lpda import LPDA
 from mylib.utils.Ltokenizer import LToken
 
 from .type import S, R
 
 
-class LParserBase(LStateMachine[S, R]):
+class LParserBase(LPDA[S, R]):
     """
     Abstract base class for Parsers.
     解析器的抽象基类
-    Inherits from LStateMachine and processes a list of LToken.
-    继承于 LStateMachine, 并处理 LToken 列表
+    Inherits from LPDA (FSM + Stack) and processes a list of LToken.
+    继承于 LPDA, 并处理 LToken 列表
     """
     def __init__(self, initial_state: S):
         super().__init__(initial_state)
