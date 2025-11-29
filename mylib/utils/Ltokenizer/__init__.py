a5551736f04c05e368c7d34d7c03753e94eb943c
Sakurine <yichengxin7@gmail.com>
2025-11-29 15:02:37 +0800

    feat(Ltokenizer): Tokenizer的抽象基类

---
+++ mylib/utils/Ltokenizer/__init__.py
@@ -0,0 +1,4 @@
+from .token import LToken, TokenType
+from .base import LTokenizerBase
+
+__all__ = ["LToken", "TokenType", "LTokenizerBase"]
---
+++ mylib/utils/Ltokenizer/type.py
@@ -0,0 +1,4 @@
+from typing import TypeVar
+from enum import Enum
+
+S = TypeVar("S", bound=Enum)
